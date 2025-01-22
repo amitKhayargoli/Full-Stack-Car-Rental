aed28ffd459c5e521194890073da111662d0e86f
0050505ishan <tikadhami77@gmail.com>
2025-01-22 15:45:08 +0545

    niceu

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Signup from "./signup";
 import Settings from "./Settings";
 import Booking from "./Booking";
 import AdminDashboard from "./AdminDashboard";
+import CustomerDashboard from "./CustomerDashboard";
+import Try from "./try";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path="/Settings" element={<Settings />} />
         <Route path="/Booking" element={<Booking />} />
         <Route path="/AdminDashboard" element={<AdminDashboard/>}/>
+        <Route path="/CustomerDashboard" element={<CustomerDashboard/>}/>
+        <Route path="/Try" element={<Try/>}/>
       </Routes>
     </BrowserRouter>
   );
