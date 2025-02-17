78f5adafb40f2af425e242017e08752956698c95
Aryan Nakarmi <133214621+pikachucodes0@users.noreply.github.com>
2025-02-17 22:15:25 +0545

    Delete view directory

--- view/auth/authRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { authController } from "../../controller/auth/authController.js";
-
-const router = express.Router();
-router.get("/init", authController.init);
-router.post("/login", authController.login);
-
-export { router as authRouter };
