e6ab56d59f48c68d1f801ca7eadadda0f86f5172
Amit Khayargoli <86340673+amitKhayargoli@users.noreply.github.com>
2025-03-04 15:43:13 +0545

    get car rental by id

--- backend/src/controller/carRentalController.js
+++ backend/src/controller/carRentalController.js
@@ -86,4 +86,48 @@ const updateRentalStatus = async (req, res) => {
   }
 };
 
-module.exports = { addRental, getAllRentals, updateRentalStatus };
+const getRentalByUserId = async (req, res) => {
+  try {
+    const userId = Number(req.user.user.userId);
+
+    if (isNaN(userId)) {
+      return res.status(400).json({ error: "Invalid user ID format" });
+    }
+
+    const rentals = await carRental.findAll({
+      where: { userId: userId },
+      include: [
+        {
+          model: Car,
+          as: "CarDetails",
+        },
+        {
+          model: Users,
+          as: "UserDetails",
+          include: [
+            {
+              model: UserProfile,
+              as: "profile",
+            },
+          ],
+        },
+      ],
+    });
+
+    if (!rentals.length) {
+      return res.status(404).json({ error: "No rentals found for this user" });
+    }
+
+    res.status(200).json({ data: rentals });
+  } catch (error) {
+    console.error("Error fetching rentals by user:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports = {
+  addRental,
+  getAllRentals,
+  updateRentalStatus,
+  getRentalByUserId,
+};
