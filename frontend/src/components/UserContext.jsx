88209009cc63f45cdb592914f0dcc064e7c5967f
Amit Khayargoli <86340673+amitKhayargoli@users.noreply.github.com>
2025-03-04 15:40:18 +0545

    added live changes in profile picture

---
+++ frontend/src/components/UserContext.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [profilePicture, setProfilePicture] = useState("");
+
+  return (
+    <UserContext.Provider value={{ profilePicture, setProfilePicture }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
